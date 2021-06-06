613f8531e5386cbfafe6bc40a8a1077088dd20ac
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:07:47 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,23 @@
+package org.cop3330;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a noun: ");
+        String noun = in.nextLine();
+        System.out.print("Enter a verb: ");
+        String verb = in.nextLine();
+        System.out.print("Enter an adjective: ");
+        String adjective = in.nextLine();
+        System.out.print("Enter an adverb: ");
+        String adverb = in.nextLine();
+
+        String adlib = String.format("Do you %s your %s %s %s? That's hilarious!",
+                                     verb, adjective, noun, adverb);
+
+        System.out.println(adlib);
+    }
+}
